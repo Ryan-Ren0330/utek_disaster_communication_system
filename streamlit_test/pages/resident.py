ad88bb07c08fe95c18f7dcc7f4b1e5442c6d87dc
utek <iwmain@outlook.com>
2025-01-18 21:33:51 +0000

    成功提交信息

--- streamlit_test/pages/resident.py
+++ streamlit_test/pages/resident.py
@@ -2,6 +2,7 @@ import streamlit as st
 import json
 import requests
 import datetime
+import base64
 
 def resident_page():
     st.set_page_config(
@@ -46,14 +47,15 @@ def resident_page():
             severity = st.selectbox("Fire Severity", ["Minor", "Moderate", "Major"])
             photo = st.file_uploader("Upload Photos (Optional)")
             submitted = st.form_submit_button("Submit Report")
-            timestamp = datetime.datetime.now().isoformat()
-            user_id = timestamp
             if submitted:
+                timestamp = datetime.datetime.now().isoformat()
+                user_id = timestamp
+                photo_data = base64.b64encode(photo.getvalue()).decode('utf-8') if photo else None
                 report_data = {
                     "location": loc,
                     "description": desc,
                     "severity": severity,
-                    "photo": photo.getvalue() if photo else None,
+                    "photo": photo_data,
                     "timestamp": timestamp,
                     "gps": [43.651070, -79.347015],
                     "user_id": user_id,
